e75f871336d20af5f41e9ce86784a8a98f3ab75d
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2024-02-14 14:18:18 +0200

    adden nav-links

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,4 +17,12 @@ WebFont.load({
       ]
     }
   });
-  
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
